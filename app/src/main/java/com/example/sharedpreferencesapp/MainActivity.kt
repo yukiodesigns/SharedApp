b2f9a19d30715649a3bc10f19f4b00be3afdceca
Yukio <elsie.atieno@student.moringaschool.com>
2024-02-22 11:50:51 +0300

    reading data in shared pref

--- app/src/main/java/com/example/sharedpreferencesapp/MainActivity.kt
+++ app/src/main/java/com/example/sharedpreferencesapp/MainActivity.kt
@@ -8,13 +8,15 @@ import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var name: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val edit: EditText = findViewById(R.id.edit)
-        val name: TextView = findViewById(R.id.last)
+
         val btn: Button = findViewById(R.id.btn)
+        DisplaySavedText()
 
         btn.setOnClickListener(){
             val enteredText: String = edit.text.toString()
@@ -32,5 +34,12 @@ class MainActivity : AppCompatActivity() {
         editor.putString("name", enteredText)
         editor.commit()
 
+    }
+
+    //Reading data from shared pref
+    fun DisplaySavedText(){
+        val sharedPreferences: SharedPreferences = getSharedPreferences("UserName", MODE_PRIVATE)
+        val s1: String? = sharedPreferences.getString("name", "")
+        name.setText(s1)
     }
 }
